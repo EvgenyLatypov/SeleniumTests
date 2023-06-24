cb12ca01673f77dca502dc40ded211748745f58c
evgeniilatypov <latypov85@gmail.com>
2023-06-24 21:55:56 +0200

    updated the element Learn More

--- src/test/java/latypov/evgenii/seleniumproject1/MainPage.java
+++ src/test/java/latypov/evgenii/seleniumproject1/MainPage.java
@@ -12,5 +12,5 @@ public class MainPage {
     public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
     public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
     public SelenideElement searchButton = $("[data-test='site-header-search-action']");
-    public SelenideElement learnMoreButton = $x("//button[text()='Learn More']");
+    public SelenideElement learnMoreButton = $x("//button[text()='Learn Moreeeeeeeeeeeeeee']");
 }
